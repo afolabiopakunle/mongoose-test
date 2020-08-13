550250300c461ec3de96a08c71e66d84995d26e4
afolabiopakunle <opakunleafolabi@gmail.com>
2020-08-13 10:33:13 +0100

    code restarted

--- app.js
+++ app.js
@@ -4,11 +4,28 @@ mongoose.connect("mongodb://localhost/students-db", {
   useUnifiedTopology: true
 });
 
-const studentSchema = mongoose.Schema({
+const studentSchema = new mongoose.Schema({
   firstname: String,
   lastname: String,
   age: Number,
+  schoolName: String,
   active: Boolean
 });
 
 const Student = mongoose.model("Student", studentSchema);
+
+let alabi = new Student({
+  firstname: "Alabi",
+  lastname: "Johnson",
+  age: 29,
+  schoolName: "St. Gregory",
+  active: false
+});
+
+alabi.save((err, alabi) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(alabi);
+  }
+});
